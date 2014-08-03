52612ac05c2fad2cf1f615ece1928cd4226b8d9e
Omer Wazir <omer.wazir@gmail.com>
2014-08-02 19:34:34 -0700

    Hooked up target and outlets

---
+++ Quiz/BNRQuizViewController.m
@@ -0,0 +1,26 @@
+//
+//  BNRQuizViewController.m
+//  Quiz
+//
+//  Created by Omer Wazir on 8/2/14.
+//  Copyright (c) 2014 Omer Wazir. All rights reserved.
+//
+
+#import "BNRQuizViewController.h"
+
+@interface BNRQuizViewController ()
+
+@property (nonatomic,weak) IBOutlet UILabel *questionLabel;
+@property (nonatomic,weak) IBOutlet UILabel *answerLabel;
+
+@end
+
+@implementation BNRQuizViewController
+
+-(IBAction)showQuestion:(id)sender{
+}
+
+-(IBAction)showAnswer:(id)sender{
+}
+
+@end
