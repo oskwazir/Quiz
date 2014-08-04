9396f79e642e55e2e2baa8553b2d7b735404991d
Omer Wazir <omer.wazir@gmail.com>
2014-08-03 19:53:47 -0700

    Basic app works but text is messed up and then Xcode crashed

--- Quiz/BNRQuizViewController.m
+++ Quiz/BNRQuizViewController.m
@@ -10,6 +10,10 @@
 
 @interface BNRQuizViewController ()
 
+@property (nonatomic) int currentQuestionIndex;
+@property (nonatomic, copy) NSArray *questions;
+@property (nonatomic, copy) NSArray *answers;
+
 @property (nonatomic,weak) IBOutlet UILabel *questionLabel;
 @property (nonatomic,weak) IBOutlet UILabel *answerLabel;
 
@@ -17,10 +21,52 @@
 
 @implementation BNRQuizViewController
 
+-(instancetype) initWithNibName:(NSString *)nibNameOrNil
+                         bundle:(NSBundle *)nibBundleOrNil{
+    
+    //call super class init method
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    
+    if(self){
+        self.questions = @[@"Who is the leader of the Rebel Alliance",
+                           @"What is the code name of Hoth Base",
+                           @"It's a ...?"];
+        
+        self.answers = @[@"Mon Mothma",
+                         @"Echo Base",
+                         @"Trap"];
+    }
+    
+    return self;
+
+}
+
 -(IBAction)showQuestion:(id)sender{
+    
+    //go to the next question
+    self.currentQuestionIndex++;
+    
+    //if we reached the last question go back to the first question
+    if(self.currentQuestionIndex == self.questions.count){
+        self.currentQuestionIndex = 0;
+    }
+    
+    //get the current question
+    NSString *question = self.questions[self.currentQuestionIndex];
+    
+    //set the question to the question label
+    self.questionLabel.text = question;
+    
+    //reset the answer label's text
+    self.answerLabel.text = @"???";
 }
 
 -(IBAction)showAnswer:(id)sender{
+    //get the answer to the current question
+    NSString *answer = self.answers[self.currentQuestionIndex];
+    
+    //set the answer to the answer label
+    self.answerLabel.text = answer;
 }
 
 @end
