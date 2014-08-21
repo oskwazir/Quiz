47dc3a094de348c418dee697f36f16c58ab8ff78
Omer Wazir <omer.wazir@gmail.com>
2014-08-21 09:07:31 -0700

    Added objective-c folder

---
+++ objective-c/Quiz/BNRQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRQuizViewController.h
+//  Quiz
+//
+//  Created by Omer Wazir on 8/2/14.
+//  Copyright (c) 2014 Omer Wazir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRQuizViewController : UIViewController
+
+@end
---
+++ objective-c/Quiz/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quiz
+//
+//  Created by Omer Wazir on 8/2/14.
+//  Copyright (c) 2014 Omer Wazir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
